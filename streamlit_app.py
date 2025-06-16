9c91b585eb09976a1833d45795debdda5166f256
rhzphlv <72447266+rhzphlv@users.noreply.github.com>
2025-06-16 14:58:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
   ingredients_string = ''
   for fruit_choosen in ingredients_list:
     ingredients_string += fruit_choosen + ' '
-    search_on =  =pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
     st.text(search_on)
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
     #st.text(smoothiefroot_response.json())
