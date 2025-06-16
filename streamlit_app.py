acf281c0a9cb6ab89cc8fbb8eabf5c65a6d817db
rhzphlv <72447266+rhzphlv@users.noreply.github.com>
2025-06-16 15:19:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'),col('FRUIT_NAME'))
 pd_df = my_dataframe.to_pandas()
-
+st.text(pd_df)
 ingredients_list = st.multiselect(
     "Choose up to 5 Fruit",
     my_dataframe,
